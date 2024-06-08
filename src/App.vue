99d1c9905f5d5a844df27ec2aa2493afd82f7842
r.alasale <rahafalasale20@gmail.com>
2024-06-09 01:56:37 +0300

    loader

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
-</template>
+  <div>
+    <loader
+      v-if="loadPage"
+      object="#ff9633"
+      color1="#ffffff"
+      color2="#17fd3d"
+      size="9"
+      speed="2"
+      bg="#343a40"
+      objectbg="#999793"
+      opacity="90"
+      name="dots"
+      disableScrolling="false"
+    ></loader>
 
+    <v-app>
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-app>
+  </div>
+</template>
 <script>
+import { mapState } from "vuex";
 export default {
   name: "App",
+  computed: {
+    ...mapState(["loadPage"]),
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from "./router";
 import store from "./store";
 import vuetify, { i18n } from "./plugins/vuetify";
 import axios from "axios";
-
+import loader from "vue-ui-preloader";
+Vue.use(loader);
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 const conficToast = {
@@ -16,6 +17,7 @@ Vue.use(Toast, conficToast);
 Vue.config.productionTip = false;
 // Vue.use(axios);
 router.beforeEach((to, from, next) => {
+  store.state.loadPage = true;
   if (to.matched.some((record) => record.meta.requiresAuth)) {
     if (!store.getters["auth/isLogin"]) {
       next({
@@ -28,10 +30,16 @@ router.beforeEach((to, from, next) => {
   next();
 });
 
+router.afterEach((to, from) => {
+  setTimeout(() => {
+    store.state.loadPage = false;
+  }, 1300);
+});
 const app = new Vue({
   router,
   store,
   vuetify,
   i18n,
+  loader: loader,
   render: (h) => h(App),
 }).$mount("#app");
