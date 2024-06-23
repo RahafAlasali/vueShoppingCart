b0b586356a856273f69c5c20b80bbda6cd04ebfc
r.alasale <rahafalasale20@gmail.com>
2024-06-23 12:55:49 +0300

    .

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === "production" ? "/vueShoppingCart/" : "/",
-  devServer: {
-    historyApiFallback: {
-      rewrites: [{ from: /\//, to: "/404.html" }],
-    },
-  },
 });
